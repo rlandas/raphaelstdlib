558ff9b48b342a40d996895b9430444f47015ac1
Savvecentral Developer <developer@savv-e.com.au>
2015-11-19 09:47:17 +1100

    Added Http stlib

---
+++ Http.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Raphael\Stdlib;
+
+abstract class Http
+{
+
+    /**
+     * Get the IP of the client
+     *
+     * @return string
+     */
+    static public function getIp ()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            // check ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            // to check ip is pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+}
